bacddd9fc5d4c6b31860d6a28b156a35fee7aeac
Frozzie <42577917+Frozzie89@users.noreply.github.com>
2026-05-21 19:55:21 +0200

    Primevue (#3)
    * added primevue
    
    * removed hello-world vue files and cleanup
    
    * new shoppinglist vue file

---
+++ frontend/src/components/ShoppingList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+interface Product {
+    name: string;
+    quantity?: number;
+}
+
+const products = ref<Product[]>([
+    { name: 'Pâtes', quantity: 2 },
+    { name: 'Frites', quantity: 1 },
+    { name: 'Riz', quantity: 3 },
+]);
+</script>
+
+<template>
+    <h1>ShoppingList </h1>
+    <DataTable :value="products">
+        <Column field="quantity" header="#"></Column>
+        <Column field="name" header="Name"></Column>
+    </DataTable>
+
+</template>
+
+<style scoped>
+</style>
